f608e57a8f862c3f3721c81deeb51b59febe9f2c
audrey murua <audrey.murua1@gmail.com>
2022-03-03 15:14:51 -0600

    relationships exercise completed

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repositories.PostRepository;
+import com.codeup.springblog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,32 +13,36 @@ import java.util.List;
 @Controller
 public class PostController {
     private PostRepository postsDao;
+    private UserRepository usersDao;
 
-    public PostController(PostRepository postsDao) {
+    public PostController(PostRepository postsDao, UserRepository usersDao) {
         this.postsDao = postsDao;
+        this.usersDao = usersDao;
 
     }
 
     @GetMapping("/posts")
     public String showAllPosts(Model model) {
         //new array list that specifies Post class and contains Post objects
-        List<Post> allPosts = new ArrayList<>();
-        Post secondPost = new Post(2, "Second Post", "This is my second post!");
-        Post thirdPost = new Post(3, "Third Post", "This is my third post! Woah!");
+//        List<Post> allPosts = new ArrayList<>();
+//        Post secondPost = new Post(2, "Second Post", "This is my second post!");
+//        Post thirdPost = new Post(3, "Third Post", "This is my third post! Woah!");
 
         //adding post objects to the allPosts array
-        allPosts.add(secondPost);
-        allPosts.add(thirdPost);
+//        allPosts.add(secondPost);
+//        allPosts.add(thirdPost);
 
-        model.addAttribute("allPosts", allPosts);
+//        model.addAttribute("allPosts", allPosts);
+
+        model.addAttribute("allPosts", postsDao.findAll());
 
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String singlePost(@PathVariable long id, Model model) {
-        Post firstPost = new Post(1,"First Post","This is my first post!");
-        model.addAttribute("individualPost", firstPost);
+//        Post firstPost = new Post(1,"First Post","This is my first post!");
+        model.addAttribute("individualPost", postsDao.getById(id));
         return "posts/show";
     }
 
@@ -49,6 +54,7 @@ public class PostController {
     @PostMapping("/posts/create")
     public String submitCreatePost(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body) {
         Post newPost = new Post(title, body);
+        newPost.setUser(usersDao.getById(1L));
         postsDao.save(newPost);
 
         return "redirect:/posts";
@@ -56,8 +62,8 @@ public class PostController {
 
     @GetMapping("/posts/{id}/edit")
     public String showEditForm(@PathVariable long id, Model model) {
-        Post posttoEdit = postsDao.getById(id);
-        model.addAttribute("postToEdit", posttoEdit);
+        Post postToEdit = postsDao.getById(id);
+        model.addAttribute("postToEdit", postToEdit);
         return "posts/edit";
     }
 
---
+++ src/main/resources/static/user_seeder.sql
@@ -0,0 +1,3 @@
+USE springblog_db;
+
+INSERT INTO users(email, password, username) VALUE ('Zelda@Hyrule.com', 'password', 'Zelda');
